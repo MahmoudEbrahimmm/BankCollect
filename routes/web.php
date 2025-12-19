187b93c13428f832be8d30f5c436f064c149a28a
mahmoudebrahim <mahmoudasar794@gmail.com>
2025-12-19 15:32:25 +0200

    Setup Blade views, configure sidebar, and correct asset paths

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Route::get('/dashboard', function () {
@@ -18,3 +19,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/{page}',[AdminController::class,'index']);
